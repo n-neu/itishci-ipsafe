9c690516076d89c1dd45fc7bac4dcb1fbf14cbc3
hanna-jd <hanna_abogado@dlsu.edu.ph>
2026-03-13 10:11:58 +0800

    updated trademark

--- ui/src/pages/services/trademark/trademark.jsx
+++ ui/src/pages/services/trademark/trademark.jsx
@@ -10,7 +10,6 @@ export default function Trademark() {
     <div className="trademark-container">
       <h2 className="page-title">Trademark</h2>
 
-      {/* The Tab Navigation */}
       <div className="tab-buttons">
         <button
           className={`tab-btn ${activeTab === "In Person" ? "yellow-tab" : "inactive-tab"}`}
@@ -26,13 +25,11 @@ export default function Trademark() {
         </button>
       </div>
 
-      {/* The White Content Card */}
       <div className="content-card">
         <h3>Fill up Info</h3>
-        {/* Your form fields and future OCR logic will go inside here */}
+        {/*OCR logic will go inside here */}
       </div>
 
-      {/* The Floating Action Button (Yellow +) */}
       <button className="fab-button">
         <svg xmlns="http://www.w3.org/2000/svg" height="40" viewBox="0 -960 960 960" width="40" fill="#FFFFFF">
           <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/>
@@ -42,4 +39,5 @@ export default function Trademark() {
       {/* Note: We will add the "Take Photo / File Upload" pop-up logic later! */}
     </div>
   );
+
 }
